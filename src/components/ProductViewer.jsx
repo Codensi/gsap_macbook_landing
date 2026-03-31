f03cae2b9d88a7141f3b9b4c1579f0f150c35e32
Nensi Golgota <nensigolgota@hotmail.com>
2026-03-31 23:49:39 +0200

    Implement 3D Product Viewer Section

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,20 +1,21 @@
 import clsx from "clsx";
 import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import MacbookModel14 from "./3d-models/Macbook-14";
-import StudioLight from "./StudioLight";
+import StudioLight from "./three/StudioLight";
+import ModelSwitcher from "./three/ModelSwitcher";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
 
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
   return (
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
 
       <div className="controls">
         <p className="info">
-          MacBook Pro {scale} in {color}.
+          MacBook Pro | Available in 14" & 16" in Space Gray and Space Black.
         </p>
 
         <div className="flex-center gap-5 mt-5">
@@ -65,9 +66,11 @@ const ProductViewer = () => {
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
         <StudioLight />
-        <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
 
-        <OrbitControls enableZoom={false} />
+        <ModelSwitcher
+          scale={isMobile ? scale - 0.03 : scale}
+          isMobile={isMobile}
+        />
       </Canvas>
     </section>
   );
